cc3a475bbdef9b07a8759030125d6f510c0d3537
mdh550 <mehdihosseini5500@gmail.com>
2021-01-22 23:29:55 +0330

    update box and its paraph

---
+++ js/code.js
@@ -0,0 +1,6 @@
+// var clickLi = document.querySelector('.nav-item');
+// window.onclick = function(event) {
+//   if (event.target == clickLi) {
+//     clickLi.classList.add("active");
+//   }
+// }
