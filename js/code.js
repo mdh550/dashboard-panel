e4a736e75d71f29f5faabab5afe89dd3d619e397
mdh550 <mehdihosseini5500@gmail.com>
2021-01-25 00:33:15 +0330

    update box hover and js functions

--- js/code.js
+++ js/code.js
@@ -1,6 +1,13 @@
-// var clickLi = document.querySelector('.nav-item');
-// window.onclick = function(event) {
-//   if (event.target == clickLi) {
-//     clickLi.classList.add("active");
-//   }
-// }
+function overBox (box,p,h) {
+    document.querySelector(box).style.border= "1px solid #3751FF";
+    document.querySelector(box).style.boxShadow= "0px 0px 10px rgba(55, 81, 255,.11)";
+    document.querySelector(p).style.color= "#3751FF";
+    document.querySelector(h).style.color= "#3751FF";
+}
+
+function outBox (box,p,h) {
+    document.querySelector(box).style.border= "1px solid #DFE0EB";
+    document.querySelector(box).style.boxShadow= "";
+    document.querySelector(p).style.color= "#9FA2B4";
+    document.querySelector(h).style.color= "#252733";
+}
