926a528cb2c9cc6f0a747458af0eeda14b2526d7
mdh550 <mehdihosseini5500@gmail.com>
2021-01-25 15:57:17 +0330

    correcting chart style and adding data

--- js/covidchart.js
+++ js/covidchart.js
@@ -1,29 +1,41 @@
-const ctx = document.getElementById("covidchart");
+const ctx = document.getElementById("covidchart").getContext('2d'),
+        gradientC = ctx.createLinearGradient(0, 0, 980,0);
+
+gradientC.addColorStop(0, 'rgba(55, 81, 255, 0.1)');
+gradientC.addColorStop(1, 'rgba(55, 81, 255, 0)');
+
+
+
 if (ctx){
     new Chart(ctx, {
         type: 'line',
-        data: {
-        labels: ["2005", "2007" , "2009" , "2012", "2014"],
-        datasets: [
-        { label: 'HouseHold income',
-        data: [5000,8000,10000,1200,15000],
-        backgroundColor :['rgba(255, 129, 102, 1)',
-        'rgba(234, 162, 235, 1)',
-        'rgba(255, 206, 36, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        ],
-        }
-        ]
-        },
-        options: {
-        scales: {
-        yAxes: [{
-        ticks: {
-        beginAtZero:true
-        }
-        }]
-        }
-        }
-        });
+  data: {
+    labels: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22],
+    datasets: [{ 
+        data: [12,20,25,28,29],
+        borderColor: "#3751FF",
+        borderWidth:'2',
+        pointBorderColor: 'rgba(0, 0, 0, 0)',
+        pointBackgroundColor: 'rgba(0, 0, 0, 0)',
+        fill: true,
+        backgroundColor: gradientC
+      }, { 
+        data: [10,20,50,30,20,10,5,9],
+        pointBorderColor: 'rgba(0, 0, 0, 0)',
+        pointBackgroundColor: 'rgba(0, 0, 0, 0)',
+        borderColor: "#DFE0EB",
+        fill: false
+      }
+    ]
+  },
+  options: {
+    legend: {
+        position:'right',
+        display: false,
+     },
+    title: {
+      display: false,
+    }
+  }
+    });
 }
